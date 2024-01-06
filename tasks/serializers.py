73fc41e0969c2bcd0e99b22f5c2065c57a88233a
Federico Navós <fedenavos@gmail.com>
2024-01-06 12:49:22 -0300

    Add is_urgent and improve serializer

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -2,6 +2,21 @@ from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
+    is_urgent = serializers.SerializerMethodField()
+
     class Meta:
         model = Task
-        fields = ['id', 'name', 'description', 'estimate', 'state']
+        fields = ['id', 'name', 'description', 'estimate', 'state', 'is_urgent']
+
+    def validate_estimate(self, value):
+        if value < 0:
+            raise serializers.ValidationError("The estimate can't be negative")
+        return value
+    
+    def validate_state(self, value):
+        if value not in Task.STATE_CHOICES:
+            raise serializers.ValidationError("The state is not valid")
+        return value
+    
+    def get_is_urgent(self, obj):
+        return obj.estimate < 4
